f066cc8f1e9e83aa29bb688349ace10fe4207f31
neuecc <ils@neue.cc>
2013-04-05 02:25:10 +0900

    add TestProject

---
+++ CloudStructures.Tests/Config.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CloudStructures.Redis;
+using System.Threading.Tasks;
+using System.Text;
+using System.Threading;
+
+namespace CloudStructures.Tests
+{
+    public static class GlobalSettings
+    {
+        public static readonly RedisSettings Default = new RedisSettings("127.0.0.1", ioTimeout: 5);
+    }
+}
