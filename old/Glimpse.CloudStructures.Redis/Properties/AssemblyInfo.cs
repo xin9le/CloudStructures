9a860c9f67a4c449ea94e4f4ee0dbede452e64be
Takaaki Suzuki <xin9le@live.jp>
2019-02-19 18:06:19 +0900

    Move all files to old directory

---
+++ old/Glimpse.CloudStructures.Redis/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Glimpse.CloudStructures.Redis")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Glimpse.CloudStructures.Redis")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1c32ba7d-44ca-45a0-a7c1-303156ab00b7")]
+
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
