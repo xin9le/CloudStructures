db3f58489b5c2ed1a29397350a7b81df433575e4
Takaaki Suzuki <xin9le@live.jp>
2019-02-20 15:45:31 +0900

    Add NuGet packaging batch file

---
+++ nuget/pack.bat
@@ -0,0 +1,3 @@
+dotnet pack ../src/CloudStructures/CloudStructures.csproj -c Release -o ../../nuget/packages
+dotnet pack ../src/CloudStructures.Converters.MessagePack/CloudStructures.Converters.MessagePack.csproj -c Release -o ../../nuget/packages
+dotnet pack ../src/CloudStructures.Converters.Utf8Json/CloudStructures.Converters.Utf8Json.csproj -c Release -o ../../nuget/packages
