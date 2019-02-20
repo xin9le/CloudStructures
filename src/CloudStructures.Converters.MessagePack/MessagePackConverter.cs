3250a2a85373ee80c87bf194036b3145c660d376
Takaaki Suzuki <xin9le@live.jp>
2019-02-20 15:32:24 +0900

    Add MessagePack for C# converter project

---
+++ src/CloudStructures.Converters.MessagePack/MessagePackConverter.cs
@@ -0,0 +1,32 @@
+﻿using MessagePack;
+
+
+
+namespace CloudStructures.Converters
+{
+    /// <summary>
+    /// Provides value converter using MessagePack for C#.
+    /// </summary>
+    public sealed class MessagePackConverter : IValueConverter
+    {
+        /// <summary>
+        /// Serialize value to binary.
+        /// </summary>
+        /// <typeparam name="T">Data type</typeparam>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public byte[] Serialize<T>(T value)
+            => LZ4MessagePackSerializer.Serialize(value);
+
+
+        /// <summary>
+        /// Deserialize value from binary.
+        /// </summary>
+        /// <typeparam name="T">Data type</typeparam>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public T Deserialize<T>(byte[] value)
+            => LZ4MessagePackSerializer.Deserialize<T>(value);
+    }
+}
+
