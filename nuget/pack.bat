9fc664dd54c9b27a04a8e2f160b78cf2b3417d59
Takaaki Suzuki <xin9le@live.jp>
2021-04-29 17:15:38 +0900

    Remove MessagePack converter project from pack.bat.

--- nuget/pack.bat
+++ nuget/pack.bat
@@ -1,2 +1,1 @@
 dotnet pack ../src/CloudStructures/CloudStructures.csproj -c Release -o ../nuget/packages
-dotnet pack ../src/CloudStructures.Converters.MessagePack/CloudStructures.Converters.MessagePack.csproj -c Release -o ../nuget/packages
