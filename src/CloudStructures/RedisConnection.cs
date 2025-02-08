9aa4c8308db9c8b0d136c0263999a1f8e8c3ab1f
Takaaki Suzuki <xin9le@live.jp>
2025-02-08 23:27:44 +0900

    Use System.Threading.Lock

--- src/CloudStructures/RedisConnection.cs
+++ src/CloudStructures/RedisConnection.cs
@@ -154,9 +154,13 @@ public sealed class RedisConnection(
         }
     }
 
+#if NET9_0_OR_GREATER
+    private readonly System.Threading.Lock _gate = new();
+#else
     private readonly object _gate = new();
+#endif
     private ConnectionMultiplexer? _connection;
-    #endregion
+#endregion
 
 
     /// <summary>
